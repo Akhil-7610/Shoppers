626f48bea7133219f7dbfb7924ec70bf513bbc8f
Akhil-7610 <akhilverma.av78@gmail.com>
2025-02-21 13:44:04 +0530

    first commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: ["class"],
+    content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+  	extend: {
+  		colors: {
+  			'rabbit-red': '#ea2e0e'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+}
+
