5b1223d448b8cc0ac3cd83e0258d679463166183
Akhil-7610 <akhilverma.av78@gmail.com>
2025-02-21 13:49:47 +0530

    first commit

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -24,7 +24,7 @@ const cartSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
     },
-    questId: {
+    guestId: {
         type: String,
     },
     products: [cartItemSchema],
